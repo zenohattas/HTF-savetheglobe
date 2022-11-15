657bd00a436945b792224d622d17826dc96a867a
Arvo cant <aro.vant@student.ap.be>
2022-11-15 10:23:32 +0100

    Added Login Functionality

--- savetheglobe-front-end/src/index.js
+++ savetheglobe-front-end/src/index.js
@@ -3,13 +3,31 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Auth0Provider } from "@auth0/auth0-react";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <Auth0Provider
+  domain="dev-iv2wvcfy528etoco.us.auth0.com"
+  clientId="4aNIUPyNnu2VZWJl9up0Z4JTHj4mMxOH"
+  redirectUri={window.location.origin}
+  >
     <App />
+  </Auth0Provider>,
   </React.StrictMode>
 );
+
+// ReactDOM.render(
+//   <Auth0Provider
+//   domain="dev-iv2wvcfy528etoco.us.auth0.com"
+//   clientId="4aNIUPyNnu2VZWJl9up0Z4JTHj4mMxOH"
+//   redirectUri={window.location.origin}
+//   >
+//     <App />
+//   </Auth0Provider>,
+//   document.getElementById("root")
+// );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
