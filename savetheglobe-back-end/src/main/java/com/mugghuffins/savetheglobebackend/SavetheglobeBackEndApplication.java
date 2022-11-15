f2bc4efc0e5777c28e4587e591c972dcccea5b85
Zeno Hattas <zeno.hattas@ap.student.be>
2022-11-15 10:07:05 +0100

    Add backend-project

---
+++ savetheglobe-back-end/src/main/java/com/mugghuffins/savetheglobebackend/SavetheglobeBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.mugghuffins.savetheglobebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SavetheglobeBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SavetheglobeBackEndApplication.class, args);
+	}
+
+}
---
+++ savetheglobe-back-end/src/test/java/com/mugghuffins/savetheglobebackend/SavetheglobeBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mugghuffins.savetheglobebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SavetheglobeBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
