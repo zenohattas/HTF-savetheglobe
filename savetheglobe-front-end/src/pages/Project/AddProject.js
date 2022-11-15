40ad4b89bfdc694225ebf77d9fa528def87cbcfd
Zeno Hattas <zeno.hattas@ap.student.be>
2022-11-15 16:06:49 +0100

    Commiting final changes

--- savetheglobe-front-end/src/pages/Project/AddProject.js
+++ savetheglobe-front-end/src/pages/Project/AddProject.js
@@ -1,82 +1,101 @@
 import axios from 'axios';
-import React, { useState } from 'react'
-import { Link, useNavigate } from 'react-router-dom';
+import React, { useEffect, useState } from 'react'
+import { Link, useNavigate, useParams } from 'react-router-dom';
 
 export default function Addproject() {
 
-  let navigate=useNavigate()
+  let navigate = useNavigate();
 
-  const [project, setproject] = useState({
-    projectType: "",
-    description: "",
-    picturePath: "",
-    supporters: ""
+  const { id } = useParams();
+
+  const [projectcontribution, setproject] = useState({
+    project: "",
+    amountContributed: ""
   });
 
-  const { projectType, projectname, amount } = project
+  const [projectList, setprojects] = useState({
+    projects: []
+  });
+
+  const { project, amountContributed } = projectcontribution
 
   const onInputChange = (e) => {
-    setproject({ ...project, [e.target.name]: e.target.value })
+    setproject({ ...projectcontribution, [e.target.name]: e.target.value })
   }
+
+  useEffect(() => {
+    loadproject();
+    loadprojects();
+  }, []);
 
   const onSubmit = async (e) => {
     e.preventDefault();
-    await axios.post("http://localhost:8080/project", project);
+    await axios.put(`http://localhost:8080/projectcontribution/${id}`, projectcontribution);
     navigate("/");
 
+  }
+
+  const loadproject = async () => {
+    const result = await axios.get(`http://localhost:8080/projectcontribution/${id}`)
+    setproject(result.data);
+  }
+
+  const loadprojects = async () => {
+    const result = await axios.get(`http://localhost:8080/projectcontribution/${id}`)
+    setproject(result.data);
   }
 
   return (
     <div className='container'>
       <div className='row'>
         <div className="col-md-6 offset-md-3 border rounded p-4 mt-2 shadow">
-          <h2 className='text-center m-4'>Register project</h2>
+          <h2 className='text-center m-4'>Edit project contribution</h2>
 
-          <form onSubmit={(e)=> onSubmit(e)}>
-            <div className='mb-3'>
-              <label htmlFor='projectType' className='form-label'>
-                Soort Project
-              </label>
-              <input
-                type={"text"}
-                className="form-control"
-                placeholder='bv. bomen, planten, etc'
-                name='projectType'
-                value={projectType}
-                onChange={(e) => onInputChange(e)} />
-            </div>
-
+          <form onSubmit={(e) => onSubmit(e)}>
             <div className='mb-3'>
               <label htmlFor='projectname' className='form-label'>
                 projectname
               </label>
-              <input
-                type={"text"}
-                className="form-control"
-                placeholder='Enter your projectname'
-                name='projectname'
-                value={projectname}
-                onChange={(e) => onInputChange(e)} />
+              <div class="dropdown">
+                <button
+                  class="btn btn-secondary dropdown-toggle"
+                  type="button"
+                  data-bs-toggle="dropdown"
+                  aria-expanded="false"
+                  value={project}
+                  placeholder="Choose you project">
+
+                </button>
+                <ul class="dropdown-menu">
+                  {
+                    projectList.projects.map((project, index) => (
+                      <li><a class="dropdown-item" href="#" onClick={(e) => onInputChange(e)}>{project.projectname}</a></li>
+                    ))
+                  }
+                </ul>
+              </div>
+              <div className='mb-3'>
+                <label htmlFor='Project' className='form-label'>
+                  Amount Contributed
+                </label>
+                <input
+                  type={"text"}
+                  className="form-control"
+                  placeholder='Enter your amount to contribute'
+                  name='amountContributed'
+                  value={amountContributed}
+                  onChange={(e) => onInputChange(e)} />
+              </div>
+
+
             </div>
 
-            <div className='mb-3'>
-              <label htmlFor='amount' className='form-label'>
-                amount
-              </label>
-              <input
-                type={"text"}
-                className="form-control"
-                placeholder='Enter the amount you want to add'
-                name='amount'
-                value={amount}
-                onChange={(e) => onInputChange(e)} />
-            </div>
             <button type='sumbit' className='btn btn-outline-primary'>Submit</button>
             <Link className='btn btn-outline-danger mx-2' to="/">Cancel</Link>
           </form>
         </div>
-      </div>
-    </div>
+      </div >
+    </div >
 
   )
 }
